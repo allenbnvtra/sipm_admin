64d954c22136bed0bd3b0422a38bb332c8ea1970
Allen Buenaventura <115561893+allenbnvtra@users.noreply.github.com>
2024-08-19 02:48:21 +0800

    d

--- api/config/database.js
+++ api/config/database.js
@@ -2,10 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   await mongoose
-    .connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    .connect(process.env.MONGO_URI)
     .then(() => {
       console.log('Connected to Database');
     })
--- api/index.js
+++ api/index.js
@@ -24,6 +24,7 @@ app.use(express.json({ limit: '10kb' }));
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   })
 );
