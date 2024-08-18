ad87ca0ea89d0fe71ac8d1bd0018cd3d32fa64fa
Allen Buenaventura <115561893+allenbnvtra@users.noreply.github.com>
2024-08-19 01:59:59 +0800

    db

--- api/config/database.js
+++ api/config/database.js
@@ -2,7 +2,10 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   await mongoose
-    .connect(process.env.MONGO_URI)
+    .connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .then(() => {
       console.log('Connected to Database');
     })
